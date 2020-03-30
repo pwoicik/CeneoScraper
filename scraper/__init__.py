889a85e676e31bd1d8e1fb105c9ee573935b5abe
pwoicik <patrykwoicik@gmail.com>
2020-03-30 17:55:40 +0200

    setting up flask

---
+++ scraper/__init__.py
@@ -0,0 +1,24 @@
+import os
+
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "scraper.sqlite")
+    )
+
+    app.config.from_pyfile("config.py", silent=True)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/")
+    def index():
+        return "hi"
+
+    return app
