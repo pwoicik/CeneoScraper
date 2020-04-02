f5d099dc7e6c2cd6f692f16c1b5b9163f470f669
pwoicik <patrykwoicik@gmail.com>
2020-04-02 03:04:47 +0200

    action buttons and refactoring

--- scraper/db_manip.py
+++ scraper/db_manip.py
@@ -6,28 +6,42 @@ from flask import (
     Response
 )
 
-from .models import db, Product, Review
+from .extraction import *
+from .models import db, Product
 from .utils import to_json
 
 
 bp = Blueprint("db", __name__, url_prefix="/db")
 
 
-@bp.route("/product/<int:pid>", methods=["GET", "DELETE"])
+@bp.route("/product/<int:pid>", methods=["GET", "PUT", "DELETE"])
 def remove_product(pid: int) -> Response:
     product = Product.query.filter(Product.id == pid).first()
 
     headers = {"Content-Type": "application/json"}
 
-    if not product:
-        return make_response(jsonify({"status": "couldn't find product"}), 400, headers)
-
-    if request.method == "GET":
+    if request.method == "GET" and product:
         return make_response(to_json(product), 200, headers)
-    elif request.method == "DELETE":
-        for review in product.reviews:
-            db.session.delete(review)
-        db.session.delete(product)
+
+    elif request.method == "PUT":
+        if product:
+            delete_product(product)
+
+        product = extract(pid)
+        db.session.add(product)
         db.session.commit()
 
-    return make_response(jsonify({"status": "ok"}), 200, headers)
+        return make_response(jsonify({"status": "ok"}), 201, headers)
+
+    elif request.method == "DELETE" and product:
+        delete_product(product)
+        return make_response(jsonify({"status": "ok"}), 200, headers)
+
+    return make_response(jsonify({"status": "couldn't find product"}), 400, headers)
+
+
+def delete_product(product: Product):
+    for review in product.reviews:
+        db.session.delete(review)
+    db.session.delete(product)
+    db.session.commit()
---
+++ scraper/static/action_buttons.js
@@ -0,0 +1,28 @@
+async function updateProductInfo(button) {
+    button.classList.add("updating");
+
+    const pid = button.getAttribute("data-pid");
+    await fetch(`/db/product/${pid}`, {method: "PUT"});
+    location.reload();
+}
+
+async function downloadProductInfo(button) {
+    const pid = button.getAttribute("data-pid");
+
+    const json = fetch(`/db/product/${pid}`).then(res => res.json());
+    const file = new Blob(
+        [JSON.stringify(json, null, 4)],
+        {type: "application/json;charset=utf-8"}
+    );
+
+    const virtual_btn = document.createElement('a');
+    virtual_btn.href = URL.createObjectURL(file);
+    virtual_btn.download = `${pid}.json`;
+    virtual_btn.click();
+}
+
+async function deleteProductAndLeave(button) {
+    const pid = button.getAttribute("data-pid");
+    await fetch(`/db/product/${pid}`, {method: "DELETE"});
+    location.assign("/products");
+}
